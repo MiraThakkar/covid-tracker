87e77421c347a4f0b3892b21d80092994187e467
Mira Thakkar <mthakkar84@gmail.com>
2020-04-20 17:40:06 -0400

    sidebar and navbar added to the home page

---
+++ src/components/SubMenu/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css"; 
+
+function Sidebar() {
+
+    return(
+
+        <div className="wrapper">
+
+            <nav id="sidebar">
+                <div className="sidebar-header">
+                    <h3>Dashboard</h3>
+                </div>
+            </nav>
+        </div>
+
+    );
+}
+
+export default Sidebar;
