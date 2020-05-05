946d882236af491945296d8c33bae3cd4f59fdf2
Ouro <ourosalimabdul@gmail.com>
2020-05-05 18:37:23 -0400

    Comment authentication code

--- models/user.js
+++ models/user.js
@@ -27,14 +27,19 @@ const UserSchema = new Schema({
     required: "Password is Required",
     validate: [({ length }) => length >= 8, "Password should be longer."]
   },
-
-  // userCreated: {
-  //   type: Date,
-  //   default: Date.now
-  // }
+  
 });
 
 const User = mongoose.model("User", UserSchema);
+
+// User.prototype.validPassword = function(password) {
+//   return bcrypt.compareSync(password, this.password);
+// };
+// // Hooks are automatic methods that run during various phases of the User Model lifecycle
+// // In this case, before a User is created, we will automatically hash their password
+// User.addHook("beforeCreate", function(user) {
+//   user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+// });
 
 module.exports = User;
 
