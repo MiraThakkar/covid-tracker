6b59d5b7260dec09014d734962abc094da7398f7
Mira Thakkar <mthakkar84@gmail.com>
2020-05-09 18:08:38 -0400

    saved server.js

--- server.js
+++ server.js
@@ -24,16 +24,9 @@ app.use(passport.session());
 app.use(routes);
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 
-<<<<<<< HEAD
 // // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trackerDB");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_2jh5m5w6:6nr8e8jg9gam3map9ojv5bj6nm@ds141960.mlab.com:41960/heroku_2jh5m5w6");
-=======
-// // Connect to the Mongo DB some
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trackerDB");
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_px71prjl:blasT123!@ds133428.mlab.com:33428/heroku_px71prjl");
-
->>>>>>> fb9a5c456fb0e4047c9f464d3244da590eec42dc
 
 // Start the API server
 app.listen(PORT, () => {
